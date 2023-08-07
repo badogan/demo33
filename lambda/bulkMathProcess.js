f7e903af8d8803edd36ab3c9af96ca1f544f8f72
Basri Dogan <basri.dogan.71@gmail.com>
2023-08-07 18:50:32 +0100

    trying again

---
+++ lambda/bulkMathProcess.js
@@ -0,0 +1,27 @@
+exports.handler = async function (event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  let receivedNumbers = JSON.parse(event.body).inputArray
+  let receivedNumbersArray = receivedNumbers.split(',').map(item => parseFloat(item))
+  let summed = receivedNumbersArray.reduce((sum, num) => sum + num)
+  let multiplied = receivedNumbersArray.reduce((sum, num) => sum * num)
+  // let body = `Hello from lamda! \n Original numbers: ${receivedNUmbersArray} \n The sum is ${summed} \n The multiplication is ${multiplied} \n Enjoy`
+  let bodyObj = Object.create({})
+  bodyObj.receivedNumbersArray = receivedNumbersArray
+  bodyObj.summed = summed
+  bodyObj.multiplied = multiplied
+  bodyObj.basriTestKey = "BasriTestValue"
+  bodyObj.ardaTestKey = "ArdaTestValue"
+
+  // `{summed:${summed},multiplied:${multiplied}}`
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST'
+    },
+    body: JSON.stringify(bodyObj)
+  };
+};
